d3f0bd0601cb04c44a37c3cefbd1394e9ae3e88e
Gediminas Paulauskas <menesis@pov.lt>
2012-08-28 17:41:37 +0300

    Set version to 2.1.1 Remove credits.rst (now included in about.rst)

--- source/conf.py
+++ source/conf.py
@@ -45,9 +45,9 @@ copyright = '2012, the Shuttleworth Foundation'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '2.0'
+version = '2.1'
 # The full version, including alpha/beta/rc tags.
-release = '2.0.1'
+release = '2.1.1'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
